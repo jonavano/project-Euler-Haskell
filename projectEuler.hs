5e2c29846020d94935f93b7393a701a2a9ab05ae
Jonathan <jonathanvanoudheusden@gmail.com>
2022-07-01 07:27:53 +0000

    Add problem 2: fibonacci

--- projectEuler.hs
+++ projectEuler.hs
@@ -1,16 +1,19 @@
-import Distribution.Simple.Utils (xargs)
+
 dividesBy3Or5 :: [Integer]
 dividesBy3Or5 = [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 
 multiplesOf3Or5 :: Integer
 multiplesOf3Or5 = sum dividesBy3Or5
 
+----
+
 fib :: Int -> Int
 fib a | a <= 1 = 1
 fib n = fib(n - 1) + fib(n - 2)
 
-fibSequence = [fib x | x <- [1..], even x]
+fibSequence = [fib x | x <- [1 .. ]]
+evenFib = filter even fibSequence
 
-lessThan4Mill = takeWhile (< 4000000) fibSequence
+lessThan4Mill = takeWhile (< 4000000) evenFib
 
 evenFibonacciNumbers = sum lessThan4Mill
